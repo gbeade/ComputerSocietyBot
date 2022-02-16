01cd3a147da46bf64382b031b713266b87fe2ca5
Gonza Beade <67883264+gbeade@users.noreply.github.com>
2022-02-16 21:47:37 +0000

    Fixed csxp bugs

--- cogs/cog_csxp.py
+++ cogs/cog_csxp.py
@@ -9,7 +9,11 @@ class Csxp(commands.Cog):
 
     @commands.group(name="csxp", invoke_without_command=True) 
     async def csxp(self, ctx: commands.Context, msg = ""): 
-      await ctx.send("Root command: csxp")
+      await ctx.send(
+        "<@{0}> You have {1} CSXP.".format(
+          ctx.author.id, 
+          data.fetch_csxp(ctx.author.name)
+        ))
  
     @csxp.command(name="leaderboard")  # subcommand
     async def leaderboard(self, ctx: commands.Context):
@@ -17,8 +21,8 @@ class Csxp(commands.Cog):
       strn += "---------------------------------\n"
       # TODO: ordenar por cantidad de puntos y poner emojis del podio 
       for x in ctx.guild.members: 
-        strn += "[@{0}]: {1} CSXP\n".format(
-          x.name, 
+        strn += "[<@{0}>]: {1} CSXP\n".format(
+          x.id, 
           data.fetch_csxp(x.name)
         )
       await ctx.send(strn)
@@ -61,7 +65,7 @@ class Csxp(commands.Cog):
 
     @csxp.command(name="faucet")
     async def faucet(self, ctx: commands.Context): 
-      t = inc_csxp(ctx.author.name, 1) 
+      t = data.inc_csxp(ctx.author.name, 1) 
       await ctx.send("Here's 1 CSXP! Your current balance is now: "+ str(t))
 
       
